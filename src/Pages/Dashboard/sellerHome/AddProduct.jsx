978cc8569e3f1d94c433a2a67e2c4d6dfbf73597
jahidhasan6676 <jh18186676@gmail.com>
2025-02-05 22:06:48 +0600

    product save database

--- src/Pages/Dashboard/sellerHome/AddProduct.jsx
+++ src/Pages/Dashboard/sellerHome/AddProduct.jsx
@@ -1,26 +1,83 @@
+import { toast } from "react-toastify";
+import useAuth from "../../../Hooks/useAuth";
+import useAxiosPublic from "../../../Hooks/useAxiosPublic";
+import { photoUpload } from "../../../utilities/utils";
+
 export default function AddProduct() {
+    const {user} = useAuth();
+    const axiosPublic = useAxiosPublic();
+
+    const handleAddProduct = async(e) =>{
+        e.preventDefault();
+
+        const form = e.target;
+        const productName = form.productName.value;
+        const manCategory = form.manCategory.value;
+        const productCategory = form.productCategory.value;
+        const description = form.description.value;
+        const quantity = parseInt(form.quantity.value);
+        const image = form.image.files[0];
+        const photo = await photoUpload(image);
+        const date = new Date();
+        const ownerInfo = {
+            name: user?.displayName,
+            email: user?.email,
+            photo: user?.photoURL
+        }
+
+        const addProductData = {
+            productName,
+            manCategory,
+            productCategory,
+            description,
+            quantity,
+            photo,
+            date,
+            ownerInfo
+        }
+
+        // product data save database
+        try {
+            const res = await axiosPublic.post("/product", addProductData)
+            if (res.data.insertedId) {
+                console.log(res.data)
+                toast.success("Product successfully added",{
+                    position: "top-center",
+                    autoClose: 3000,
+                })
+                form.reset();
+            }
+
+        } catch (err) {
+            toast.error(err.message,{
+                position: "top-center",
+                autoClose: 3000,
+            })
+        }
+
+    }
     return (
         <div className=" bg-gray-50 min-h-[calc(100vh-60px)]  flex justify-center items-center">
             <div className="max-w-2xl mx-auto shadow-sm bg-white p-10 rounded-md ">
                 <h2 className="text-2xl font-semibold text-gray-800 mb-6 text-center sm:text-lg">Add New Product</h2>
-                <form className="grid grid-cols-1 gap-6 sm:gap-4">
+                <form onSubmit={handleAddProduct} className="grid grid-cols-1 gap-6 sm:gap-4">
                     {/* Image Upload */}
                     <div className="col-span-2">
                         <label className="block text-sm font-medium text-gray-700">Product Image</label>
-                        <input type="file" className="mt-2 w-full p-3 border border-gray-300 focus:outline-gray-300 rounded-lg sm:p-2" />
+                        <input type="file" name="image" accept='image/*' className="mt-2 w-full p-3 border border-gray-300 focus:outline-gray-300 rounded-lg sm:p-2" />
                     </div>
 
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-6 sm:grid-cols-1">
                         {/* Product Name */}
                         <div className="w-full">
                             <label className="block text-sm font-medium text-gray-700">Product Name</label>
-                            <input type="text" placeholder="Enter product name" className="mt-2 w-full p-3 border border-gray-300 focus:outline-gray-300 rounded-lg sm:p-2 focus:ring-gray-100" />
+                            <input type="text" name="productName" placeholder="Enter product name" className="mt-2 w-full p-3 border border-gray-300 focus:outline-gray-300 rounded-lg sm:p-2 focus:ring-gray-100" />
                         </div>
 
                         {/* Main Category */}
                         <div className="w-full">
                             <label className="block text-sm font-medium text-gray-700">Main Category</label>
-                            <select className="mt-2 w-full p-3 border border-gray-300 focus:outline-gray-300 rounded-lg sm:p-2">
+                            <select name="manCategory" className="mt-2 w-full p-3 border border-gray-300 focus:outline-gray-300 rounded-lg sm:p-2">
                                 <option>Men</option>
                                 <option>Women</option>
                                 <option>Kids</option>
@@ -30,20 +87,20 @@ export default function AddProduct() {
                         {/* Product Category */}
                         <div className="w-full">
                             <label className="block text-sm font-medium text-gray-700">Product Category</label>
-                            <input type="text" placeholder="e.g. T-shirt, Jeans" className="mt-2 w-full p-3 border border-gray-300 focus:outline-gray-300 rounded-lg sm:p-2" />
+                            <input type="text" name="productCategory" placeholder="e.g. T-shirt, Jeans" className="mt-2 w-full p-3 border border-gray-300 focus:outline-gray-300 rounded-lg sm:p-2" />
                         </div>
 
                         {/* Quantity */}
                         <div className="w-full">
                             <label className="block text-sm font-medium text-gray-700">Quantity</label>
-                            <input type="number" placeholder="Enter quantity" className="mt-2 w-full p-3 border border-gray-300 focus:outline-gray-300 rounded-lg sm:p-2" />
+                            <input type="number" name="quantity" placeholder="Enter quantity" className="mt-2 w-full p-3 border border-gray-300 focus:outline-gray-300 rounded-lg sm:p-2" />
                         </div>
                     </div>
 
                     {/* Description */}
                     <div className="col-span-2">
                         <label className="block text-sm font-medium text-gray-700">Description</label>
-                        <textarea placeholder="Enter product description" className="mt-2 w-full p-3 border border-gray-300 focus:outline-gray-300 rounded-lg h-32 sm:p-2 sm:h-24"></textarea>
+                        <textarea name="description" placeholder="Enter product description" className="mt-2 w-full p-3 border border-gray-300 focus:outline-gray-300 rounded-lg h-32 sm:p-2 sm:h-24"></textarea>
                     </div>
 
                     {/* Submit Button */}
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,18 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './Router/Router'
 import AuthProvider from './Context/AuthProvider'
 import { ToastContainer } from 'react-toastify'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
-      <ToastContainer/>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ToastContainer />
+      </QueryClientProvider>
     </AuthProvider>
   </StrictMode>,
 )
